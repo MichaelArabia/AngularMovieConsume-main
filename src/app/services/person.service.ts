07b013a4fd837d67069542efb37c5afcde4ef74c
Michaël Arabia <arabia.michael@gmail.com>
2022-10-13 22:03:24 +0200

    create service - comment create Person get,post in service

--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -1,20 +1,43 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Person } from '../models/person.model';
+import { ActIn } from '../models/actorIn.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonService {
 
-  private url : string = "http://localhost:53448/api";
+  private _url : string = "http://localhost:53448/api";
 
-  constructor(private client: HttpClient) { }
+  constructor(private _client: HttpClient) { }
 
   getAllPerson(): Observable<Person[]>
   {
-    return this.client.get<Person[]>(this.url+'/person')
+    return this._client.get<Person[]>(this._url+'/person')
+  }
+
+  createPerson(person:Person):Observable<Person>{
+    let myHeaders : HttpHeaders = new HttpHeaders({
+      'Authorization' : 'Bearer ' + localStorage.getItem('token')
+    })
+    return this._client.post<Person>(this._url+"/person", person, { headers : myHeaders } )
+
+  }
+
+  getOnePerson(id:number):Observable<Person>{
+    let myHeaders : HttpHeaders = new HttpHeaders({
+      'Authorization' : 'Bearer ' + localStorage.getItem('token')
+    })
+    return this._client.get<Person>(this._url+"/person/" + id, { headers : myHeaders } )
+  }
+
+  setActorRole(setActor: ActIn):Observable<ActIn>{
+    let myHeaders : HttpHeaders = new HttpHeaders({
+      'Authorization' : 'Bearer' + localStorage.getItem('token')
+    })
+    return this._client.post<ActIn>(this._url+"/person/setActor", setActor, { headers : myHeaders})
   }
 
 
